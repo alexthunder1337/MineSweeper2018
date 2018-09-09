f53a4a9e32c5407cfdeb574566533b15f631692b
alex_thunder <s1d0r0valexiv@gmail.com>
2018-09-09 16:13:51 +0300

    Created Main Window

--- src/com/MineSweeper/MineSweeper.java
+++ src/com/MineSweeper/MineSweeper.java
@@ -6,4 +6,16 @@ public class MineSweeper extends JFrame {
     public static void main(String[] args) {
         new MineSweeper().setVisible(true);
     }
+
+    private MineSweeper(){
+        initFrame();
+    }
+    private void initFrame (){
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Mine Sweeper 2018");
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
 }
