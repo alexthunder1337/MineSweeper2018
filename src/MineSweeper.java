7978cfe905e8307de5c46b60fe90180f0f4c5330
alex_thunder <s1d0r0valeix@gmail.com>
2018-09-10 01:55:44 +0300

    Created Coord class (contains XY Coordinates for fields)

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,35 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
+
 import Sweeper.Box;
+import Sweeper.Coord;
 
 public class MineSweeper extends JFrame {
     private JPanel panel;
-    private final int COLS =15;
+    private final int COLS = 15;
     private final int ROWS = 1;
     private final int IMAGE_SIZE = 50;
+
     public static void main(String[] args) {
         new MineSweeper();
     }
-    private MineSweeper(){
+
+    private MineSweeper() {
         setImages();
         initPanel();
         initFrame();
     }
-    private void initPanel(){
-        panel = new JPanel(){
+
+    private void initPanel() {
+        panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for (Box box : Box.values())
+                for (Box box : Box.values()) {
+                Coord coord = new Coord(box.ordinal() * IMAGE_SIZE, 0);
                     g.drawImage((Image) box.image,
-                            box.ordinal() * IMAGE_SIZE, 0, this);
+                            coord.x, coord.y, this);
                 }
+            }
         };
-        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE,
-                ROWS*IMAGE_SIZE));
-        add (panel);
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE,
+                ROWS * IMAGE_SIZE));
+        add(panel);
     }
-    private void initFrame (){
+
+    private void initFrame() {
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Mine Sweeper 2018");
@@ -37,12 +45,14 @@ public class MineSweeper extends JFrame {
         setResizable(false);
         setVisible(true);
     }
-    private void setImages(){
-        for (Box box : Box.values()){
+
+    private void setImages() {
+        for (Box box : Box.values()) {
             box.image = getImage(box.name().toLowerCase());
         }
     }
-    private Image getImage (String img_name){
+
+    private Image getImage(String img_name) {
         String filename = "img/" + img_name + ".png";
         ImageIcon icon = new ImageIcon(getClass().getResource(filename));
         return icon.getImage();
