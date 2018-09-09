0ae63692dfa9a228615f4df591d1952f70d9ed80
alex_thunder <s1d0r0valexiv@gmail.com>
2018-09-09 15:12:21 +0300

    Created Main Window

---
+++ src/com/MineSweeper/MineSweeper.java
@@ -0,0 +1,9 @@
+package com.MineSweeper;
+
+import javax.swing.*;
+
+public class MineSweeper extends JFrame {
+    public static void main(String[] args) {
+        new MineSweeper().setVisible(true);
+    }
+}
